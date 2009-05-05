9092c4d3bb9bba304512c4bfe2a8297bd13dfb7c
Christian Rodemeyer <crodemeyer@tigris.org>
2009-05-05 19:57:27 +0000

    added release download 0.6.2 and more documentation
    git-svn-id: http://svnquery.tigris.org/svn/svnquery/trunk@134 ef9fb88f-4736-4558-9cff-406f806169d0

--- source/SvnIndex/Properties/AssemblyInfo.cs
+++ source/SvnIndex/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("SvnQuery")]
+[assembly: AssemblyTitle("SvnIndex")]
 [assembly: AssemblyDescription("Create and update a lucene index for subversion")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -52,7 +52,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.6.1.0")]
+// [assembly: AssemblyVersion("0.6.2.0")]
 
-[assembly: AssemblyVersion("0.6.1.0")]
-[assembly: AssemblyFileVersion("0.6.1.0")]
+[assembly: AssemblyVersion("0.6.2.0")]
+[assembly: AssemblyFileVersion("0.6.2.0")]
